9baeb583adecdcac6b99ac7b5c7e2916346e7fbf
Szilard Udroiu <uszilard@gmail.com>
2019-12-19 21:52:07 +0100

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -5,20 +5,20 @@ var dotenv = require("dotenv");
 dotenv.config();
 
 var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "cat_db"
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "cat_db"
 });
 
 // Make connection.
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
