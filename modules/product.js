b5b14cb9a136039bd7548bb27702c1f0d8a84e73
SabineWanka <sabinewanka@yahoo.de>
2020-01-11 10:32:36 +0100

    adding order module

--- modules/product.js
+++ modules/product.js
@@ -24,32 +24,6 @@ var product = {
         });
     }
 };
-var order = {
-    all: function (cb) {
-        orm.all("orders", function (res) {
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
-        orm.create("orders", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    update: function (objColVals, condition, cb) {
-        orm.update("orders", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (condition, cb) {
-        orm.delete("orders", condition, function (res) {
-            cb(res);
-        });
-    }
-};
-
 // Export the database functions for the controller (catsController.js).
 module.exports = product;
-module.exports = order;
 
-// question - can i put the order thing in theproduct.js file or should I create an extra file for that ?
