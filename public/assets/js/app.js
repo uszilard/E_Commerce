2e97ff016b6224e7bd33894f3f27bdbc4acd2f8c
Szilard Udroiu <uszilard@gmail.com>
2020-01-14 21:02:32 +0100

    small changes to app.js

---
+++ public/assets/js/app.js
@@ -0,0 +1,123 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".add-to-cart").on("click", function (event) {
+        event.preventDefault()
+        console.log("i am finnalaly doin someting")
+        var id = $(this).data("id");
+
+        var newOrder = $(this).data("neworder");
+
+        var userAsString = JSON.stringify({
+            key: "id",
+            id
+        });
+
+        localStorage.setItem("cart", userAsString);
+
+
+
+        // Send the PUT request.
+        $.ajax("/api/cart/" + id, {
+            type: "PUT",
+        }).then(
+            function () {
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("check-out", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newOrder = {
+            name: $("#ca").val().trim(),
+
+        };
+
+        // Send the POST request.
+        $.ajax("/api/cart", {
+            type: "POST",
+            data: newOrder
+        }).then(
+            function () {
+                console.log("created new order");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-order").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/cart/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted order", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".change-cart").on("click", function (event) {
+        var id = $(this).data("id");
+        var newProduct = $(this).data("newproduct");
+
+
+
+        // Send the PUT request.
+        $.ajax("/api/cart" + id, {
+            type: "PUT",
+        }).then(
+            function () {
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newProduct = {
+            name: $("#ca").val().trim(),
+
+        };
+
+        // Send the POST request.
+        $.ajax("/api/products", {
+            type: "POST",
+            data: newProduct
+        }).then(
+            function () {
+                console.log("created new product");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-product").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/products/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted product", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8089;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
